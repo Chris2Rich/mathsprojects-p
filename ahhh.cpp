4a6c2fcb49beaa34a7c4051ab60e784f974ca497
Chris2Rich <AzkronTheGamer@gmail.com>
2023-09-02 23:14:51 +0100

    made functions rely on pointers instead of duplicated values.

--- ahhh.cpp
+++ ahhh.cpp
@@ -5,11 +5,11 @@
 template <typename T>
 using vector = std::vector<T>; 
 
-double DotProduct(vector<double> A, vector<double> B){
+double DotProduct(vector<double>* A, vector<double>* B){
     double total = 0;
-    if(A.size()==B.size()){
-        for(int i = 0; i<A.size();i++){
-            total+= A[i] * B[i];
+    if(A->size()==B->size()){
+        for(int i = 0; i<A->size();i++){
+            total+= A->operator[](i) * B->operator[](i);
         }
     }
     else{
@@ -19,42 +19,43 @@ double DotProduct(vector<double> A, vector<double> B){
     return total;
 }
 
-vector<double> FindColumn(vector<vector<double>> Matrix, int column){
+vector<double> FindColumn(vector<vector<double>>* Matrix, int column){
     vector<double> answer;
-    for(int i = 0; i < Matrix.size();i++){
-        answer.push_back(Matrix[i][column]);
+    for(int i = 0; i < Matrix->size();i++){
+        answer.push_back(Matrix->operator[](i)[column]);
     }
     return answer;
 }
 
-vector<vector<double>> MatrixByScalar(double A, vector<vector<double>> B)
+vector<vector<double>> MatrixByScalar(vector<vector<double>>* B, double A)
 {
-    for (int i = 0; i < B.size(); i++)
+    for (int i = 0; i < B->size(); i++)
     {
-        for (int j = 0; j < B[i].size(); j++)
+        for (int j = 0; j < B->operator[](i).size(); j++)
         {
 
-            B[i][j] = B[i][j] * A;
+            B->operator[](i)[j] = B->operator[](i)[j] * A;
 
         }
     }
-    return B;
+    return *B;
 }
 
-//Throws error unless matrix A is longer and matrix B is taller or they are of same size
-vector<vector<double>> MatrixByMatrix(vector<vector<double>> A, vector<vector<double>> B)
+//Throws error unless matrix A is longer and matrix B is taller or they are of same size -> Matrix Multiplication is not commutative.
+//Memory usage should maximise here because of new memory being allocated to matrix C -> Could be a potential bottleneck in applications that repeatedly call.
+vector<vector<double>> MatrixByMatrix(vector<vector<double>>* A, vector<vector<double>>* B)
 {
 
-    vector<vector<double>> C(A.size(),vector<double>(B[0].size()));
+    vector<vector<double>> C(A->size(),vector<double>(B->operator[](0).size()));
 
-    if (A[0].size() == B.size())
+    if (A->operator[](0).size() == B->size())
     {
-        for (int i = 0; i < A.size(); i++)
+        for (int i = 0; i < A->size(); i++)
         {
-            for (int j = 0; j < B[i].size(); j++)
+            for (int j = 0; j < B->operator[](i).size(); j++)
             {
-
-                C[i][j] = DotProduct(A[i],FindColumn(B,j));
+                vector<double> BRow = FindColumn(B,j);
+                C[i][j] = DotProduct(&A->operator[](i),&BRow);
 
             }
         }
@@ -66,11 +67,12 @@ vector<vector<double>> MatrixByMatrix(vector<vector<double>> A, vector<vector<do
     return C;
 }
 
-vector<vector<double>> MatrixRotate2D(vector<vector<double>> Matrix, double Angle){
-    if(Matrix.size()==2||Matrix[0].size()==2){
+vector<vector<double>>* MatrixRotate2D(vector<vector<double>>* Matrix, double Angle){
+    if(Matrix->size()==2||Matrix->operator[](0).size()==2){
     Angle *= 3.14159265359/180;
     vector<vector<double>> RotationMatrix = {{cos(Angle),-sin(Angle)},{sin(Angle),cos(Angle)}};
-    return MatrixByMatrix(RotationMatrix, Matrix);
+    *Matrix = MatrixByMatrix(&RotationMatrix, Matrix);
+    return Matrix; 
     }
     else{
         std::cout << "Error: Matrix is not 2D\n";
@@ -78,11 +80,11 @@ vector<vector<double>> MatrixRotate2D(vector<vector<double>> Matrix, double Angl
     return Matrix;
 }
 
-vector<vector<double>> MatrixRotate3D(vector<vector<double>> Matrix, vector<double> direction){
-    if(Matrix.size()==3||Matrix[0].size()==3){
-        double a = direction[0];
-        double b = direction[1];
-        double c = direction[2];
+vector<vector<double>>* MatrixRotate3D(vector<vector<double>>* Matrix, vector<double>* direction){
+    if(Matrix->size()==3||Matrix->operator[](0).size()==3){
+        double a = direction->operator[](0);
+        double b = direction->operator[](1);
+        double c = direction->operator[](2);
 
         a *= 3.14159265359/360;
         b *= 3.14159265359/360;
@@ -93,7 +95,8 @@ vector<vector<double>> MatrixRotate3D(vector<vector<double>> Matrix, vector<doub
         {sin(a)*cos(b), sin(a)*sin(b)*sin(c)+cos(a)*cos(c), sin(a)*sin(b)*cos(c)-cos(a)*sin(c)},
         {-sin(b), cos(b)*sin(c), cos(b)*cos(c)}
         };
-    return MatrixByMatrix(RotationMatrix, Matrix);
+    *Matrix = MatrixByMatrix(&RotationMatrix, Matrix);
+    return Matrix;
     }
     else{
         std::cout << "Error: Matrix is not 3D\n";
@@ -101,27 +104,33 @@ vector<vector<double>> MatrixRotate3D(vector<vector<double>> Matrix, vector<doub
     return Matrix;
 }
 
-vector<vector<double>> MatrixScale(vector<vector<double>> Matrix, vector<double> direction){
-    if(Matrix.size()==direction.size()){
-    vector<vector<double>> ScaleMatrix(direction.size(),vector<double>(direction.size()));
-    for(int i=0; i < direction.size(); i++){
-        ScaleMatrix[i][i] = direction[i];
+vector<vector<double>>* MatrixScale(vector<vector<double>>* Matrix, vector<double>* direction){
+    if(Matrix->size()==direction->size()){
+    vector<vector<double>> ScaleMatrix(direction->size(),vector<double>(direction->size()));
+    for(int i=0; i < direction->size(); i++){
+        ScaleMatrix[i][i] = direction->operator[](i);
     }
-    return MatrixByMatrix(ScaleMatrix, Matrix);}
+    *Matrix = MatrixByMatrix(&ScaleMatrix, Matrix);
+    return Matrix;
+    }
     else{
         std::cout << "Error: Matrix and Vector are of different size\n";
         return Matrix;
     }
 }
 
-void PrintMatrix(vector<vector<double>> Matrix){
-    for (int i = 0; i < Matrix.size(); i++)
+vector<vector<double>>* MatrixShear(vector<vector<double>>* Matrix, vector<vector<double>>* direction){
+    return Matrix;
+}
+
+void PrintMatrix(vector<vector<double>>* Matrix){
+    for (int i = 0; i < Matrix->size(); i++)
     {
-        for (int j = 0; j < Matrix[i].size(); j++)
+        for (int j = 0; j < Matrix->operator[](i).size(); j++)
         {
 
             std::cout << "|";
-            std::cout << Matrix[i][j];
+            std::cout << Matrix->operator[](i)[j];
             std::cout << "|";
 
         }
@@ -138,11 +147,7 @@ int main()
     vector<vector<double>> MatrixC = {{1,3,4},{5,8,2},{5,7,1}};
     vector<vector<double>> MatrixD = {{1},{1}};
 
-    auto MatrixAns = MatrixRotate3D(MatrixScale(MatrixC,{1,2,1}),{180,180,0});
-    PrintMatrix(MatrixAns);
+    vector<double> Rotate3DParam = {180,180,0};
+    vector<double> ScaleParam = {1,2,1};
 
-    std::cout << "\n";
-
-    auto MatrixAns2 = MatrixRotate2D(MatrixA,90);
-    PrintMatrix(MatrixAns2);
 }
