4da84aeab776269541ec017a73dc1431ec84de43
Chris2Rich <AzkronTheGamer@gmail.com>
2023-09-13 21:36:26 +0100

    added speed

--- BenchMark.cpp
+++ BenchMark.cpp
@@ -10,16 +10,11 @@ int main(){
     float (A)[testsize];
     float (B)[testsize];
 
-    for(int i = 0; i < testsize; i++){
-        A[i] = 2;
-        B[i] = 5;
-    }
-
     clock_t start, end;
     start = clock();
     std::ios_base::sync_with_stdio(false);
 
-    auto Ans= DotProduct(&A,&B,testsize);
+    auto Ans= DotProduct(A,B,testsize);
 
     end = clock();
 
