4d0f071799ed33a53ab2fab42a01cf001590f827
Chris2Rich <AzkronTheGamer@gmail.com>
2023-09-13 20:06:11 +0100

    found out this is super slow!!! removed vector overhead in test branch

--- BenchMark.cpp
+++ BenchMark.cpp
@@ -5,7 +5,7 @@ int main(){
     time_t now = time(&now);
     srand(now);
     
-    int testsize=1024;
+    int testsize=259677;
 
     std::vector<std::vector<double>> A(testsize,std::vector<double>(testsize));
     std::vector<std::vector<double>> B(testsize,std::vector<double>(testsize));
@@ -14,16 +14,14 @@ int main(){
     std::vector<double> D(testsize);
 
     for(int i = 0; i< testsize;i++){
-        for(int j= 0; j< testsize; j++){
-            A[i][j] = rand();
-            B[i][j] = rand();
-        }
+        C[i] = 2;
+        D[i] = 5;
     }
 
     clock_t start, end;
     start = clock();
     std::ios_base::sync_with_stdio(false);
-    auto Ans = MatrixByMatrix(&A,&B);
+    auto Ans = DotProduct(&C,&D);
     end = clock();
 
     double total_time = double(end-start) / double(CLOCKS_PER_SEC);
