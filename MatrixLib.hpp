e8cd742a0aa6db7f22912957fb1814527b59d61d
Chris <143294640+Chris2Rich@users.noreply.github.com>
2023-09-14 12:53:59 +0100

    Update MatrixLib.hpp, removed AVX512 due to bleeding edge not being wise for general library
    eeeee MIT >>>>

--- MatrixLib.hpp
+++ MatrixLib.hpp
@@ -8,11 +8,11 @@ const unsigned int thread_count = std::thread::hardware_concurrency();
 
 inline float DotProduct(float* A, float* B, int size){
     float ans = 0;
-    for(int i = 0; i < size; i+=16){
-        __m512 VectA = _mm512_load_ps(A+i);
-        __m512 VectB = _mm512_load_ps(B+i);
-        __m512 VectAns = _mm512_mul_ps(VectA,VectB);
-        for(int j = 0; j < 16; j++){
+    for(int i = 0; i < size; i+=4){
+        __m128 VectA = _mm_load_ps(A+i);
+        __m128 VectB = _mm_load_ps(B+i);
+        __m128 VectAns = _mm_mul_ps(VectA,VectB);
+        for(int j = 0; j < 4; j++){
             ans += VectAns[j];
         }
     }
