ba340d42dab3ce48330929dda8840b7b0ce08212
Chris2Rich <AzkronTheGamer@gmail.com>
2024-01-09 22:12:36 +0000

    ,

--- 1brc/gen.cpp
+++ 1brc/gen.cpp
@@ -1,7 +1,26 @@
+#include <iostream>
 #include <fstream>
+#include <random>
+#include <string>
+#include <vector>
 
 int main(){
     std::ofstream file("./.txt");
 
+    srand((unsigned) time(0));
+    std::vector<std::string> names((rand() % 9999) + 1);
+
+    //Creating Name Vector Loop
+    // for(int i = 0; i < names.size(); i++){
+    //     names[i] = 
+    // }
+
+    //Writing Loop
+    // for(int i = 0; i < 0x3B9ACA00; i++){
+
+    // }
+
     file.close();
+    int test[] {67, 69, 85, 85};
+    std::cout << (char) &test;
 }
